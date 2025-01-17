ec36869d06655d9e81b5e05ca8e9e11ecdc2bf5e
synthelz <atrandon29@jacksonindians.us>
2025-01-17 00:06:31 -0600

    Update stats.js

--- stats.js
+++ stats.js
@@ -1,10 +1,10 @@
-const molds = ['Basic', 'Bronze', 'Gold'];
-const qualities = ['Normal', 'Awesome', 'Perfect'];
-const rarities = ['Common', 'Legendary', 'Omnificent'];
+const molds = ['Basic', 'Bronze', 'Gold', 'Platinum', 'Diamond'];
+const qualities = ['Normal', 'Awesome', 'Perfect', 'Superior', 'Masterpiece'];
+const rarities = ['Common', 'Rare', 'Epic', 'Legendary', 'Mythical', 'Omnificent'];
 
-const moldMultipliers = [1, 2, 4];
-const qualityMultipliers = [1, 2, 4];
-const rarityMultipliers = [1.1, 2, 5];
+const moldMultipliers = [1, 2, 4, 6, 8];
+const qualityMultipliers = [1, 2, 4, 6, 8];
+const rarityMultipliers = [1.1, 1.5, 2, 3, 4, 5];
 
 function generateSword(moldLevel, qualityLevel, rarityLevel) {
     const mold = molds[Math.floor(Math.random() * moldLevel)];
