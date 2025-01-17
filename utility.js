cd64add2d92ca40c05fdec75f6db1b257fa4c96a
synthelz <atrandon29@jacksonindians.us>
2025-01-17 00:00:38 -0600

    Create utility.js

---
+++ utility.js
@@ -0,0 +1,8 @@
+function updateStats(money, swordsMade, lastSword) {
+    document.getElementById('money').innerText = money;
+    document.getElementById('swords-made').innerText = swordsMade;
+    if (lastSword) {
+        document.getElementById('last-sword').innerText = `${lastSword.mold}, ${lastSword.quality}, ${lastSword.rarity}`;
+    }
+    console.log("Stats updated");
+}
